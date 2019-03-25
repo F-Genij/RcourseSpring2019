c8556d4978b173fd647f12e19b63e60e7cf030d7
laurafontanesi <laura.fontanesi.1@gmail.com>
2019-03-25 21:24:54 +0100

    added generating script for data_wp4 assignment

---
+++ R/data_wpa4.R
@@ -0,0 +1,14 @@
+# Load data
+matthews.df <- read.csv(file = "~/git/RcourseSpring2019/data/data_wpa4.csv",
+                        header = TRUE)
+
+# Create demo.df
+set.seed(100)
+demo.df <- data.frame(id = matthews.df$id)
+demo.df$height <- round(rnorm(n = nrow(demo.df), mean = 170, sd = 10), 0)
+demo.df$race <- sample(c("white", "hispanic", "black", "asian"), size = nrow(demo.df), prob = c(.5, .1, .2, .2), replace = TRUE)
+
+demo.df <- demo.df[sample(nrow(demo.df)),]
+rownames(demo.df) <- 1:nrow(demo.df)
+
+write.csv(x = demo.df, file = "~/git/RcourseSpring2019/data/matthews_demographics.csv")
